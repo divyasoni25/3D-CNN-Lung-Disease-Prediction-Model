aad6397b98702a21309cbb920d5969e43a0e6256
divyasoni25 <143200499+divyasoni25@users.noreply.github.com>
2024-06-28 11:38:05 +0530

    Update 3D_image_classification.py

--- 3D_image_classification.py
+++ 3D_image_classification.py
@@ -1,13 +1,4 @@
 """
-Title: 3D image classification from CT scans
-Author: [Hasib Zunair](https://twitter.com/hasibzunair)
-Date created: 2020/09/23
-Last modified: 2024/01/11
-Description: Train a 3D convolutional neural network to predict presence of pneumonia.
-Accelerator: GPU
-"""
-
-"""
 ## Introduction
 
 This example will show the steps needed to build a 3D convolutional neural network (CNN)
